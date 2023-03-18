8d965fdb36110ef3c1300e278be920f53d0925d9
Aman <117364369+AmanPaliye@users.noreply.github.com>
2023-03-18 10:20:19 +0530

    Add files via upload

---
+++ Mouse Event/index.js
@@ -0,0 +1,12 @@
+const containerEl = document.querySelector(".container");
+
+window.addEventListener("mousemove", () => {
+  containerEl.innerHTML = `<div class="mouse-event">
+ ${event.clientX}
+ <h4>Mouse X Position(px)</h4>
+</div>
+<div class="mouse-event">
+ ${event.clientY}
+ <h4>Mouse Y Position(px)</h4>
+</div>`;
+});
