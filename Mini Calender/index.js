7dfab5a12a143963a13434b13b02ce49b2708aa1
Aman <117364369+AmanPaliye@users.noreply.github.com>
2023-03-20 18:55:32 +0530

    Add files via upload

---
+++ Mini Calender/index.js
@@ -0,0 +1,20 @@
+const monthNameEl = document.getElementById("month-name");
+
+const dayNameEl = document.getElementById("day-name");
+
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerHTML = date.toLocaleString("en", {
+  month: "long",
+});
+
+dayNameEl.innerText = date.toLocaleString("en", {
+  weekday: "long",
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
